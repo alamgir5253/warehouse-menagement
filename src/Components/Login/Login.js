f47130532e122c35f0ab6c26428f3e99264991d9
alomgir hossain <alamgir5253@.com>
2022-05-01 00:34:07 +0600

    login page signup page style done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { FaFacebook,FaGoogle } from "react-icons/fa";
+
 import './Login.css'
 const Login = () => {
   return (
-    <div>
-      <h1>login</h1>
+    <section>
+      
+      <div className='form-container'>
+      <form >
+      <h2 className='form-title'>login</h2>
+        <div className="input-container">
+          <input type="text" name="" id="" placeholder='enter email' />
+          <input type="text" name="" id="" placeholder='enter password'/>
+        </div>
+        <p>forget password?</p>
+        <button type='submit'>Login</button>
+        <div className="social-signup">
+        <h5>Or Signup Using </h5>
+        <FaGoogle className='social' />
+      </div>
+      </form>
+      
     </div>
+    </section>
   );
 };
 
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { FaFacebook,FaGoogle } from "react-icons/fa";
 import './SignUp.css'
 const SignUp = () => {
   return (
-    <div>
-      <h1>SignUp</h1>
+    <section>
+      
+      <div className='form-container'>
+      <form >
+      <h2 className='form-title'>signup</h2>
+        <div className="input-container">
+          <input type="text" name="" id="" placeholder='enter email' />
+          <input type="text" name="" id="" placeholder='enter password'/>
+          <input type="text" name="" id=""  placeholder='confirm password'/>
+        </div>
+        <p>forget password?</p>
+        <button type='submit'>Signup</button>
+        <div className="social-signup">
+        <h5>Or Signup Using </h5>
+        <FaGoogle className='social' />
+      </div>
+      </form>
+      
     </div>
+    </section>
   );
 };
 
