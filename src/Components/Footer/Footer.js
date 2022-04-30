80009bb89fb02d90c3f0e2966454be3965668508
alomgir hossain <alamgir5253@.com>
2022-04-30 22:18:06 +0600

    footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { FaFacebook, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
 import './Footer.css'
 const Footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
-    </div>
+    <footer className='footer-section'>
+      <div className='section1'>
+        <h3>company name</h3>
+        <div className="footer-social">
+        <FaFacebook className='facebook'/><FaTwitter className='twitter' /><FaInstagram className='instagram' /> <FaYoutube className='youtube' />
+          </div>
+      </div>
+      <div className='section2'>
+        <h3>contact info</h3>
+        <p>+1 (212)-695-1962</p>
+        <p>Info@Coffeekup.Com</p>
+        <p>463 7th Ave, NY 10018, USA</p>
+      </div>
+      <div className='section3'>
+        <h3>payment method</h3>
+
+      </div>
+    </footer>
   );
 };
 
